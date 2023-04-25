88a7c337b24dd4258ee102ce6c2d7c4057ddf494
ChaitaliV <91143017+ChaitaliV@users.noreply.github.com>
2023-04-26 01:50:19 +0530

    Update model.py

--- hate_speech/model.py
+++ hate_speech/model.py
@@ -27,8 +27,8 @@ class Encoder(tf.keras.Model):
 class HFEncoder(tf.keras.Model):
     def __init__(self):
         super(HFEncoder,self).__init__()
-        self.preprocess_layer = AutoTokenizer.from_pretrained(hf_preprocessor,max_length=512, truncation=True)
-        self.transformer_encoder = TFAutoModel.from_pretrained(hf_encoder,from_pt=True,max_length=512)
+        self.preprocess_layer = AutoTokenizer.from_pretrained(hf_preprocessor,max_length=128, truncation=True)
+        self.transformer_encoder = TFAutoModel.from_pretrained(hf_encoder,from_pt=True,max_length=128)
 
     def call(self,input):
         layer_out = self.preprocess_layer(input,return_tensors="tf", padding=True)
